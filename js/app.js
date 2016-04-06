dd96ba545b0f8178ca063889d0f9ccfd1778c5a6
muuze49 <matthewlopez@Matthews-MacBook-Pro.local>
2016-04-05 18:42:15 -0700

    hw_Matt_city

--- js/app.js
+++ js/app.js
@@ -8,13 +8,24 @@ $(document).ready(function() {
     $('#city-type').val('');
     city = city.toLowerCase().trim();
 
-    if (city == 'new york' || "nyc") {
+    if (city == 'new york' || city == 'nyc') {
       $('body').attr('class','nyc');
   }
-
-    else if (city == "san fransisco") {
+      else if (city == "san francisco" || city == 'sf') {
       $('body').attr('class','sf');
-    }
+      console.log('sf chosen')
+  }
+      else if (city == "sydney") {
+        $('body').attr('class', 'sydney')
+      }
+
+      else if (city == "los angeles" || city == 'la') {
+        $('body').attr('class', 'la')
+      }
+
+      else if (city == "austin") {
+        $('body').attr('class', 'austin')
+      }
 
   });
 
